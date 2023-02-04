9aea898c29e17b7de92fc07d734352cbca8ed6c7
Ibrahim Duman <dumanibrahm@gmail.com>
2023-02-04 15:03:53 +0300

    App created.

---
+++ ZarApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZarApp
+//
+//  Created by İbrahim Duman on 4.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
